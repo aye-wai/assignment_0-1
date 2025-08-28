791998219a6eed9d9daa937fde0e0feb7a807aef
atodorov284 <atodorov284@gmail.com>
2025-08-28 11:33:10 +0200

    feat: fix main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
-def add_two_integer_lists(a: list[int], b: list[str]) -> int:
-    arr=np.array(a)+np.array(b)
+
+
+def add_two_integer_lists(a: list[int], b: list[int]) -> None:
+    arr = np.array(a) + np.array(b)
     print(f"My added array: {arr}")
+
+
 if __name__ == "__main__":
-    add_two_integer_lists([1,2,3],[4,5,6])
+    add_two_integer_lists([1, 2, 3], [4, 5, 6])
